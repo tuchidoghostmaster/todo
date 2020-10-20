92e4c9fb6229308a5a7d74acf7bc7c6cda114d6b
TuChiDo <tuchido@TuChiDos-MacBook-Pro-2.local>
2020-10-20 15:20:13 +0700

    todo app react js

---
+++ build/precache-manifest.fac2b787b07d543863a3b07b0d631772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa3e21494809815c0fff7278b7ea11f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c84b40eab33b3cfc81",
+    "url": "/static/css/main.b4285d88.chunk.css"
+  },
+  {
+    "revision": "829010b31ecb1ecae15e",
+    "url": "/static/js/2.7d631e5d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7d631e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c84b40eab33b3cfc81",
+    "url": "/static/js/main.266a636e.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  const firstRender = useRef(true);
+
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    
+    setTodos([...todos, {
+      text: inputValue,
+      id: uuidv4()
+    }]);
+
+    setInputValue('');
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  useEffect(() => {
+    if(firstRender.current) {
+      firstRender.current = false;
+    } else {
+      localStorage.setItem("Todo", JSON.stringify([...todos]));
+    }
+  }, [todos])
+
+  useEffect(() => {
+    const newTodos = localStorage.getItem("Todo");
+    if(newTodos) {
+      setTodos(JSON.parse([...todos, newTodos]));
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <form onSubmit={addTodo}>
+          <input 
+            autoFocus
+            type="text" 
+            placeholder="Add a task..."
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {todos.map((todo) => (
+          <div key={todo.id} className="todo">
+            <p>{todo.text}</p>
+            <i onClick={() => removeTodo(todo.id)} className="far fa-trash-alt"></i>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
